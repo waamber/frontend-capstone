6ad672170c9b1078f2af0d858e5c12df056de1f2
Waamber <amberastuart@aol.com>
2017-12-08 09:57:23 -0600

    my hides printing to page

--- javascripts/controllers/MyHidesCtrl.js
+++ javascripts/controllers/MyHidesCtrl.js
@@ -1,6 +1,17 @@
 'use strict';
 
 geoApp.controller("MyHidesCtrl", function ($q, $http, $scope, FIREBASE_CONFIG, HiddenByService) {
-  
+
+  $scope.hiddenByMe = [];
+
+  const getHides = () => {
+    HiddenByService.getAllHidden().then((results) => {
+      $scope.hiddenByMe = results;
+    }).catch((error) => {
+      console.log("Error in getFinds", error);
+    });
+  };
+
+  getHides();
 
 });
