bc65f73425b602c77b7500fe86843c0455e2b756
Waamber <amberastuart@aol.com>
2017-12-12 20:36:02 -0600

    added auth background

--- javascripts/controllers/DashboardCtrl.js
+++ javascripts/controllers/DashboardCtrl.js
@@ -4,6 +4,153 @@ geoApp.controller("DashboardCtrl", function ($location, $rootScope, $scope, $win
 
   const uid = AuthService.getCurrentUid();
 
+  let styleArray = [
+    {
+      "featureType": "all",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": "-100"
+        },
+        {
+          "gamma": "0.50"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.neighborhood",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#059960"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#f57f27"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "saturation": "0"
+        },
+        {
+          "gamma": "0.50"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#02758c"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    }
+  ];
+
   $window.navigator.geolocation.getCurrentPosition(function (position) {
     $scope.map.center.latitude = position.coords.latitude;
     $scope.map.center.longitude = position.coords.longitude;
@@ -18,9 +165,10 @@ geoApp.controller("DashboardCtrl", function ($location, $rootScope, $scope, $win
       longitude: -86.787422
     },
     zoom: 11,
+    options: { styles: styleArray }
   };
 
-  $scope.option = {};
+  $scope.options = {};
 
   $scope.onClick = function (markers, eventName, model) {
     model.show = !model.show;
@@ -31,7 +179,9 @@ geoApp.controller("DashboardCtrl", function ($location, $rootScope, $scope, $win
       $scope.caches = results;
       $scope.caches.forEach((cache) => {
         if (cache.hiddenBy === uid) {
-          cache.option = { url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png' };
+          cache.option = { url: 'https://i.imgur.com/mZ0x58b.png' };
+        } else {
+          cache.option = { url: 'https://i.imgur.com/wdnLOyk.png' };
         }
       });
       FoundByService.getMyFinds(uid).then((results) => {
@@ -41,7 +191,9 @@ geoApp.controller("DashboardCtrl", function ($location, $rootScope, $scope, $win
             if (foundCache.cacheId === cache.id) {
               cache.foundId = foundCache.uid;
               if (cache.foundId === uid) {
-                cache.option = { url: 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png' };
+                cache.option = { url: 'https://i.imgur.com/KDzY85c.png' };
+              } else {
+                cache.option = { url: 'https://i.imgur.com/wdnLOyk.png' };
               }
             }
           });
@@ -63,5 +215,8 @@ geoApp.controller("DashboardCtrl", function ($location, $rootScope, $scope, $win
   $scope.goToHides = () => {
     $location.path('/hide');
   };
+
+
+
 
 });
